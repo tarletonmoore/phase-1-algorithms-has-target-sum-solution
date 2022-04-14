840a80b13fecad6ad42d9049c54f703b183cedee
John Tarleton Moore <tarletonmoore@gmail.com>
2022-04-14 14:11:13 -0400

    commit tree ec2c797b231bcf3545466240ed8bca95a5d15758

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {}
+  for (const number of array) {
+    const compliment = target - number
+    if (seenNumbers[compliment]) return true
+    seenNumbers[number] = true
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: O(n)
 */
 
 /* 
   Add your pseudocode here
+  loop through to get target number - array number to equal another array number 
+  if the loop can't do this, then it is false
 */
 
 /*
   Add written explanation of your solution here
+  This takes the array into an object. It takes the target number and loops through the other numbers subtracting them from the target and if there is another number in the loop that equals the result it will return true. This is due to the key value pair in the object, the key being the number and the value being true.
 */
 
 // You can run `node index.js` to view these console logs
